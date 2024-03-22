db6c38b191bd0e5bc2e3ba2977104753db18cc1a
gitaujustus <jgathiru02@gmail.com>
2024-03-22 22:40:55 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
+});
+
+export default withPWA({
+  reactStrictMode: true,
+});
+
